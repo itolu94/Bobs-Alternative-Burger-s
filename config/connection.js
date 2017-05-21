7a02fcee4a5d224b67a0681d428d20d693bbcf72
Tolu Idowu <itolu94@gmail.com>
2017-05-21 19:38:04 -0400

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,17 @@ var mysql = require('mysql');
 
 var mysqlCredentials = require('./mysql.js');
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'bobs_alternative_burgersdb'
-});
+var connection
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+	  host: 'localhost',
+	  user: 'root',
+	  password: '',
+	  database: 'bobs_alternative_burgersdb'
+  });
+}
 
 module.exports = connection; 

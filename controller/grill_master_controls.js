d57b80e0e46f926c04a6ebb4058373720eccaca2
Tolu Idowu <itolu94@gmail.com>
2017-05-25 18:55:54 -0400

    heroku

--- controller/grill_master_controls.js
+++ controller/grill_master_controls.js
@@ -1,45 +1,51 @@
 var express = require('express');
 var router = express.Router();
 var query = require('../config/orm.js')
-
+var burgers = {
+    eaten: [{
+            ordered_burger: 'Chicken Noodle'
+        },
+        { ordered_burger: 'Double Cheese' }
+    ],
+    notEaten: [{
+        ordered_burger: 'Thia Curry'
+    }, {
+        ordered_burger: 'Pad Thai'
+    }]
+};
 router.get('/', function(req, resp) {
-	var burgers = {
-	eaten: [],
-	notEaten: []
-    }
-	query.madeBurgers(function(result) {
-		for(var i = 0; i < result.length; i ++){
-			if(!result[i].burger_eaten){
-				console.log('Burger fount');
-				burgers.eaten.push(result[i]);
-			}	else burgers.notEaten.push(result[i]); 
-		}
-		// console.log(result);
-		// console.log(burgers);
-		resp.render('index', {eaten: burgers.eaten, 
-							  notEaten: burgers.notEaten})
-	});
+
+    query.madeBurgers(function(result) {
+        // for(var i = 0; i < result.length; i ++){
+        // 	if(!result[i].burger_eaten){
+        // 		burgers.eaten.push(result[i]);
+        // 	}	else burgers.notEaten.push(result[i]); 
+        // }
+        resp.render('index', {
+            eaten: burgers.eaten,
+            notEaten: burgers.notEaten
+        })
+    });
 });
 
 
 router.put('/:id', function(req, resp) {
-	var id = req.params.id;
-	query.eatingBurger(id);
-	resp.redirect('/');
+    var id = req.params.id;
+    query.eatingBurger(id);
+    resp.redirect('/');
 });
 
 
-router.post('/', function(req, resp){
-	var burger = req.body.burger_type;
-	query.makeBurger(burger);
-	resp.redirect('/');
+router.post('/', function(req, resp) {
+    var burger = req.body.burger_type;
+    query.makeBurger(burger);
+    resp.redirect('/');
 });
 
-router.delete('/:id', function(req, resp){
-	var id = req.params.id;
-	console.log('request DELETE made'); 
-	query.cleanPlate(id);
-	resp.redirect('/');
+router.delete('/:id', function(req, resp) {
+    var id = req.params.id;
+    query.cleanPlate(id);
+    resp.redirect('/');
 })
 
 

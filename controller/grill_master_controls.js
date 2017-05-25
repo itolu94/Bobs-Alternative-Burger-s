08e9a4b9cf0047171b00b9beac83a556474693b6
Tolu Idowu <itolu94@gmail.com>
2017-05-25 19:31:11 -0400

    heroku

--- controller/grill_master_controls.js
+++ controller/grill_master_controls.js
@@ -4,7 +4,7 @@ var query = require('../config/orm.js')
 
 router.get('/', function(req, resp) {
 	query.madeBurgers(function(result) {
-		resp.render('index', {eaten: result[0]})
+		resp.render('index', {eaten: result[0]})
 	});
 });
 
